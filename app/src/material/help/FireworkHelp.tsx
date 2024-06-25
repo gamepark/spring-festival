b67991ba438c2cda46a543a1e8822308d7242486
Patrick Beaume <p.beaume@gmail.com>
2024-06-25 09:07:25 +0200

    Composition help

--- app/src/material/help/FireworkHelp.tsx
+++ app/src/material/help/FireworkHelp.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import { MaterialComponent, MaterialHelpProps } from '@gamepark/react-game'
+import { MaterialComponent, MaterialHelpProps, usePlayerId, usePlayerName } from '@gamepark/react-game'
 import { isBaseFirework } from '@gamepark/spring-festival/material/Firework'
 import { fireworkDescriptions } from '@gamepark/spring-festival/material/FireworkDescription'
 import { LocationType } from '@gamepark/spring-festival/material/LocationType'
@@ -11,6 +11,9 @@ import { Trans } from 'react-i18next'
 export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
   const { item } = props
   const locationType = item.location?.type
+  const playerId = usePlayerId()
+  const itsMe = playerId && playerId === item.location?.player
+  const name = usePlayerName(item.location?.player)
 
   return (
     <>
@@ -19,9 +22,14 @@ export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
         <Trans defaults="help.firework.text"
                values={{ number: fireworkDescriptions[item.id.front].explosions.length, color: fireworkDescriptions[item.id.front].color }}/>
       </p>
-      {locationType === LocationType.FireworksStore && (
+      {locationType === LocationType.FireworksStorePile && (
         <p>
           <Trans defaults="help.firework.location.store"/>
+        </p>
+      )}
+      {locationType === LocationType.Panorama && (
+        <p>
+          <Trans defaults={itsMe? "help.panorama.mine": "help.panorama.player"} values={{ player: name }}/>
         </p>
       )}
       {!item.location?.rotation && (
