af4338c95f2d2bdbf66bad6784c0989255aee0ea
Patrick Beaume <p.beaume@gmail.com>
2024-06-25 09:15:21 +0200

    add applause token

--- app/src/material/help/ApplauseTokenHelp.tsx
+++ app/src/material/help/ApplauseTokenHelp.tsx
@@ -1,8 +1,30 @@
 /** @jsxImportSource @emotion/react */
-import { MaterialHelpProps } from '@gamepark/react-game'
+import { MaterialHelpProps, usePlayerId, usePlayerName } from '@gamepark/react-game'
+import { LocationType } from '@gamepark/spring-festival/material/LocationType'
 import { FC } from 'react'
+import { Trans } from 'react-i18next'
 
 export const ApplauseTokenHelp: FC<MaterialHelpProps> = (props) => {
-  console.log(props)
-  return null
+  const { item } = props
+  const playerId = usePlayerId()
+  const itsMe = playerId && playerId === item.location?.player
+  const name = usePlayerName(item.location?.player)
+  return (
+    <>
+      <h2>
+        <Trans defaults="help.applause"/>
+      </h2>
+      <p>
+        <Trans defaults="help.applause.vp"/>
+      </p>
+      <p>
+        <Trans defaults="help.applause.vp-for-less"/>
+      </p>
+      { item.location?.type === LocationType.PlayerApplause && (
+        <p>
+          <Trans defaults={itsMe ? "help.applause.count" : "help.applause.count.player"} values={{ player: name, number: item.quantity ?? 0 }}/>
+        </p>
+      )}
+    </>
+  )
 }
--- app/src/material/help/FireworkHelp.tsx
+++ app/src/material/help/FireworkHelp.tsx
@@ -1,23 +1,25 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { MaterialComponent, MaterialHelpProps, PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
-import { isMoveItemType, isStartSimultaneousRule } from '@gamepark/rules-api'
+import { isCustomMoveType, isMoveItemType, isStartSimultaneousRule } from '@gamepark/rules-api'
 import { isBaseFirework } from '@gamepark/spring-festival/material/Firework'
 import { fireworkDescriptions } from '@gamepark/spring-festival/material/FireworkDescription'
 import { LocationType } from '@gamepark/spring-festival/material/LocationType'
 import { MaterialType } from '@gamepark/spring-festival/material/MaterialType'
+import { CustomMoveType } from '@gamepark/spring-festival/rules/CustomMoveType'
 import { SpringFestivalRules } from '@gamepark/spring-festival/SpringFestivalRules'
 import { FC } from 'react'
 import { Trans } from 'react-i18next'
 
 export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
-  const { item, closeDialog } = props
+  const { item, itemIndex, closeDialog } = props
   const rules = useRules<SpringFestivalRules>()!
   const locationType = item.location?.type
   const playerId = usePlayerId()
   const itsMe = playerId && playerId === item.location?.player
   const name = usePlayerName(item.location?.player)
   const rotateToHere = useLegalMove((move) => isMoveItemType(MaterialType.FireworksStore)(move) && move.location.rotation === item.location?.id)
+  const grandeFinale = useLegalMove((move) => isCustomMoveType(CustomMoveType.GrandeFinale)(move) && move.data === itemIndex)
   const validateRotation = useLegalMove((move) => isStartSimultaneousRule(move) && !rotateToHere)
   const pileCount = locationType === LocationType.FireworksStorePile && item.location?.id !== undefined ? rules.material(MaterialType.Firework).location(LocationType.FireworksStorePile).locationId(item.location.id).length: undefined
   console.log(rotateToHere)
@@ -30,12 +32,12 @@ export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
       </p>
       {locationType === LocationType.FireworksStorePile && (
         <p>
-          <Trans defaults="help.firework.location.store"/>
+          <Trans defaults="help.firework.store"/>
         </p>
       )}
       {locationType === LocationType.Panorama && (
         <p>
-          <Trans defaults={itsMe? "help.panorama.mine": "help.panorama.player"} values={{ player: name }}/>
+          <Trans defaults={itsMe? "help.firework.panorama.mine": "help.firework.panorama.player"} values={{ player: name }}/>
         </p>
       )}
       {!item.location?.rotation && (
@@ -60,7 +62,7 @@ export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
       )}
       { rotateToHere && (
         <div>
-          <Trans defaults="help.store.rotate">
+          <Trans defaults="help.store.rotation.here">
             <PlayMoveButton move={rotateToHere} onPlay={closeDialog} />
           </Trans>
         </div>
@@ -72,15 +74,22 @@ export const FireworkHelp: FC<MaterialHelpProps> = (props) => {
           </Trans>
         </div>
       )}
+      { grandeFinale && (
+        <div>
+          <Trans defaults="help.firework.grande-finale">
+            <PlayMoveButton move={grandeFinale} onPlay={closeDialog} />
+          </Trans>
+        </div>
+      )}
       {!!pileCount && (
         <p>
-          <Trans defaults="help.pile.count" values={{ number: pileCount }}/>
+          <Trans defaults="help.firework.pile.count" values={{ number: pileCount }}/>
         </p>
       )}
       {
-        <p>
+        <div css={css`margin-bottom: 0.1em`}>
           <MaterialComponent type={MaterialType.Firework} itemId={item.id} css={!item.location?.rotation && css`transform: rotateY(180deg)`} />
-        </p>
+        </div>
       }
     </>
   )
